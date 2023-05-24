7f3863ad190e877a217baff55996caea89211656
Alberto Santamaria <alberto.santamaria@animaeducacao.com.br>
2023-05-24 15:55:08 -0300

    Modificação de segurança

---
+++ WebAPI_EmpresaReserva/Models/Preco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_EmpresaReserva
+{
+    public class Preco
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public double PrecoVenda { get; set; }
+        public string Data { get; set; }
+
+    }
+}
